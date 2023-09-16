950842e7c223efe284529860367ce0ecb92ff39d
Sarah Leder <Trinity16201@gmail.com>
2023-09-16 19:48:39 -0400

    Added questions to HTML and JS. Also worked on styling aspects displayed.

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,62 @@ function setTime() {
   }
   setTime();
 
+const questions=[
+  {
+    question: "What is an array?",
+    choices:["Money", "One data type", "Collection of same data types","One class"],
+    answer:"Collection of same data types"
+  },
+  {
+    question: "What is an string?",
+    choices:["Sequence of characters", "Numbers", "An ID","Used for playing with dogs"],
+    answer:"Sequence of characters"
+  },
+  {
+    question: "Which item will display at the top of the page?",
+    choices:["Footer", "Paragraph", "Div","H1"],
+    answer:"H1"
+  },
+  {
+    question: "Which will change the text color in CSS?",
+    choices:["Background", "Color", "Font-weight","Font-Size"],
+    answer:"Color"
+  },
+
+]
+let currentQuestion=0;
+let score=0;
+// Function to display the current question and choices
+function displayQuestion() {
+  const questionElement = document.getElementById("question");
+  const choicesElement = document.getElementById("choices");
+questionElement.textContent = questions[currentQuestion].question;
+choicesElement.innerHTML = "";
+questions[currentQuestion].choices.forEach(choice => {
+    const button = document.createElement("button");
+    button.textContent = choice;
+    button.addEventListener("click", () => selectAnswer(choice));
+    choicesElement.appendChild(button);
+  });
+}
+function selectAnswer(choice) {
+  if (choice === questions[currentQuestion].answer) {
+    score++;
+  }
+  currentQuestion++;
+if (currentQuestion < questions.length) {
+        displayQuestion();
+      } else {
+        endQuiz();
+      }
+    }
+    function endQuiz() {
+      alert(`Quiz ended! Your score is ${score}/${questions.length}`);
+    }
+function startQuiz() {
+  displayQuestion();
+}
+window.onload = startQuiz;
 // var startButtonElement = document.querySelector(".start-quiz");
 // var isWin = false;
 // var win = document.querySelector(".win");
@@ -49,17 +105,7 @@ function setTime() {
 // //     startTimer()
 // // }
 
-// const question = [
-//     {
-//         question: "What is an array?",
-//         answers: [
-//             { text: "Money", correct: false },
-//             { text: "One Data Type", correct: false },
-//             { text: "Collection of Same Data Types", correct: true },
-//             { text: "One Class", correct: false },
 
-//         ]
-//     },
 //     {
 //         question: "What is an string?",
 //         answers: [

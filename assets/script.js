15fbcf65abfeb3d8172e9031bea51a8acf0f2536
Sarah Leder <Trinity16201@gmail.com>
2023-09-20 21:16:21 -0400

    added more content to the button, updated styling, and removed unneccesary link on the first page.

--- assets/script.js
+++ assets/script.js
@@ -97,5 +97,28 @@ function beginQuiz() {
 }
 window.onload = beginQuiz;
 
+//for scoreboard//
+var initialsInput = document.querySelector("#initialInput");
+var submitButton = document.querySelector("#submitscore");
+
+var initials = localStorage.getItem("initialinput");
+
+submitButton.addEventListener("click", function () {
+    initials=initialsInput.value 
+    console.log(initials)
+    if (initials !== "") {
+        initialsInput.textContent = initials;
+        localStorage.setItem(initials,"tempscore")
+    }
+
+});
+renderSubmission();
+
+function renderSubmission() {
+    var initials = initialsInput.value;
+    if (!initials) {
+        return;
+    }
+}
 
 
